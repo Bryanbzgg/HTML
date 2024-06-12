3c3ce11c491288b561dc5e27ec3df97132e4fd8c
Bryan <seucleito123456@gmail.com>
2024-06-12 15:15:04 -0300

    finalização

--- Aula3/js/script.js
+++ Aula3/js/script.js
@@ -8,4 +8,10 @@ trocatema = () => {
 
     document.querySelector("#perfil img").setAttribute("src", imagem);
 
+
+    const alt = islight ? "um homem com os braços abertos comemorando" : "Foto de perfil mandando beijo com camisa de time (corinthians) e cabelo arrepiado para cima." ;
+
+    document.querySelector("#perfil img").setAttribute("alt", alt);
+
+
 }
